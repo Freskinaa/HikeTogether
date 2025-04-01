2f195bb43bb90adbf355b36c62ec8455d1c910ff
Freskinaa <sfreskina@gmail.com>
2025-04-02 00:06:48 +0200

    wip(home-ui): Add top services section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./home.scss"
 import Banner from '../../components/Home/Banner/Banner'
+import TopServices from '../../components/Home/TopServices/TopServices'
 
 const Home = () => {
   return (
     <div className='home-container'>
       <Banner />
+      <TopServices />
     </div>
   )
 }
