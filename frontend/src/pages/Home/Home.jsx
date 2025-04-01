0d462b1ed40f3a851242cb1874e11144b2ea1bfa
Freskinaa <sfreskina@gmail.com>
2025-04-02 00:03:22 +0200

    wip(home-ui): Add banner

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./home.scss"
+import Banner from '../../components/Home/Banner/Banner'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <Banner />
+    </div>
   )
 }
 
