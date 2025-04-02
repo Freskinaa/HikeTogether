a966d0d7d4bf703eec556f8b0f8321900a5b92f2
Freskinaa <sfreskina@gmail.com>
2025-04-03 01:33:01 +0200

    feat: add events page

--- frontend/src/pages/Trail/SingleTrail.jsx
+++ frontend/src/pages/Trail/SingleTrail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import './singleTrail.scss'
 import TrailDetails from '../../components/Trail/TrailDetails';
@@ -7,11 +7,17 @@ import DetailsOfTrail from '../../components/Trail/DetailsOfTrail';
 import TrailRoute from '../../components/Trail/TrailRoute';
 import TrailWeather from '../../components/Trail/TrailWeather';
 import TrailEvents from '../../components/Trail/TrailEvents';
+import { getAllTrails } from '../../store/trailSlice';
 
 const SingleTrail = () => {
   const { id } = useParams();
+  const dispatch = useDispatch();
   const {trails} = useSelector(state => state.trail);
   const [trail, setTrail] = useState(null)
+
+  useEffect(() => {
+    dispatch(getAllTrails())
+  }, [dispatch])
 
   useEffect(() => {
     if(id && trails) {
