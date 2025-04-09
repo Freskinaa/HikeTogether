e60c87b60605d6fdda803fb7fa620753b8ed01ff
Erona <eronagojnovcii@gmail.com>
2025-04-09 03:02:49 +0200

    added coments

--- frontend/src/use-cases/CreateEventUseCase.js
+++ frontend/src/use-cases/CreateEventUseCase.js
@@ -1,5 +1,7 @@
 import { EventService } from "../services/EventService";
-
+// This use case handles the creation of a new event by utilizing the EventService.
+// It provides a static `execute` method that takes event data as input,
+// and delegates the creation process to the `createEvent` method in EventService.
 export class CreateEventUseCase{
   static async execute(eventData) {
     return await EventService.createEvent(eventData);
